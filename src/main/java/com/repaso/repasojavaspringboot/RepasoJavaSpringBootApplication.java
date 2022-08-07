de5d39814b934e3554a4aeb4291760ba0e949dd9
Julieth Estefania Osorio <july_24_estefa@hotmail.com>
2022-08-07 18:29:49 -0500

    commit de prueba

--- src/main/java/com/repaso/repasojavaspringboot/RepasoJavaSpringBootApplication.java
+++ src/main/java/com/repaso/repasojavaspringboot/RepasoJavaSpringBootApplication.java
@@ -8,6 +8,9 @@ public class RepasoJavaSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RepasoJavaSpringBootApplication.class, args);
+
+        System.out.println("Hola Julieth");
+
     }
 
 }

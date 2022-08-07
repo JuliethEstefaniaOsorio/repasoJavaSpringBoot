2bde6663cddfdb9162e2b3ce9cd96c01de999a56
Julieth Estefania Osorio <july_24_estefa@hotmail.com>
2022-08-07 18:21:33 -0500

    Initial project

---
+++ src/main/java/com/repaso/repasojavaspringboot/RepasoJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.repaso.repasojavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoJavaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepasoJavaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/repaso/repasojavaspringboot/RepasoJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repaso.repasojavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
